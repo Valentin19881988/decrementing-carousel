4cb57f20f3616b043b8df316d669722bed19f4b2
Valiantsin Kuzniatsou <bpssba@gmail.com>
2022-06-26 17:06:43 +0300

    almost done

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,92 @@
 package com.epam.rd.autotasks;
 
 public class CarouselRun {
+    static int n;
+    Boolean s = false;
+    Boolean isFinishChek = false;
 
     public int next() {
-       throw new UnsupportedOperationException();
-    }
 
-    public boolean isFinished() {
-        throw new UnsupportedOperationException();
-    }
 
-}
+        int sumNull = 0;
+        int sum = 0;
+        if (DecrementingCarousel.count == 0) { //проверка есть ли элеметы для уменьшения (проверка на то, что не добавили ни одного элемента)
+            s = true;
+            return -1;
+        }
+
+        for (int i = DecrementingCarousel.count - 1; i >= 0; i--) {
+            if (DecrementingCarousel.array[i] == 0)        //считаем количество нулевых элементов в массиве
+                sumNull++;
+
+            sum += DecrementingCarousel.array[i];
+            // }
+            if (i == 0 && sum == 0) {
+                s = true;
+                DecrementingCarousel.count = 0;   //когда цикл уменьшения закончен, обнуляем счётчик изначально добавленных элементов
+                n = 0; isFinishChek = true;                             //переводим отсчёт к началу массива
+                return -1;
+            }}
+            if (sumNull == DecrementingCarousel.count - 1) { //если кол-во нулевых элементов в массиве на 1 меньше количества элементов массива
+                //int tmp = DecrementingCarousel.array[n];
+                //DecrementingCarousel.array[n]--;
+                //return tmp;
+                n = 0;
+                for (int e = n; e < DecrementingCarousel.count ; e++) {  //ищем следующий ненулевой эл-т
+                    if (DecrementingCarousel.array[e] != 0) {
+                        n = e;
+                        break;
+                    }
+                }
+                int tmp = DecrementingCarousel.array[n];
+                DecrementingCarousel.array[n]--;
+                return tmp;
+
+            }                                                   //это значит что остался последний элемент масиива.
+
+
+            if (DecrementingCarousel.array[n] == 0) {                //проверяем элемент на значение 0
+                for (int e = n; e < DecrementingCarousel.count - 1; e++) {  //ищем следующий ненулевой эл-т
+                    if (DecrementingCarousel.array[e] != 0) {
+                        n = e;                               //если ненулевой элемент найден , выходим из цикла
+                        break;
+                    }
+                    n++;
+                }
+            }
+            if (n == DecrementingCarousel.count-1 && DecrementingCarousel.array[n] == 0 )      // если вся посделовательность элементов до конца матрицы ,включая последний элемент, равна 0
+                n = 0;                                        //переходим к началу матрицы
+
+            if (DecrementingCarousel.array[n] == 0) {        // повторяем поиск ненулевого элемента (уже с начала матрицы)
+                for (int e = n; e < DecrementingCarousel.count - 1; e++) {
+                    if (DecrementingCarousel.array[e] != 0) {
+                        n = e;
+                        break;
+                    }
+
+                }
+            }
+
+
+            int tmp = DecrementingCarousel.array[n];
+            if (DecrementingCarousel.array[n] != 0)
+                DecrementingCarousel.array[n]--;
+            n++;
+
+            if (n == DecrementingCarousel.count)
+                n = 0;
+            sumNull = 0;
+            return tmp;
+
+            //throw new UnsupportedOperationException();
+        }
+
+        public boolean isFinished () {
+            if (DecrementingCarousel.count == 0 || isFinishChek == true)
+                s = true;
+
+
+            return s;
+        }
+
+    }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,38 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarousel {
+    Boolean s = true, get = true, isRun = false;
+    public static int[] array = new int[0];
+    public static int count = 0;
     public DecrementingCarousel(int capacity) {
+        this.array = new int[capacity];
 
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        if (get == false)
+             return false;
+
+        if (element <= 0)
+            return false;
+
+        if (count== array.length)
+            return false;
+
+        this.array[count] = element;
+        count++;
+        return true;
+
     }
 
     public CarouselRun run(){
-       throw new UnsupportedOperationException();
+        if (isRun == true)
+            return null;
+        CarouselRun run = new CarouselRun();
+        //run.next();
+        get = false;
+        isRun = true;
+
+        return run;
     }
 }
